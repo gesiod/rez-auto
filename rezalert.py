1b657537a3d7d03aa66e102d57718ac5ad92efa1
Grigoriy Gesiod <g.gesiod@gmail.com>
2024-08-21 18:15:22 +0300

    Update rezalert.py

--- rezalert.py
+++ rezalert.py
@@ -1,22 +1,19 @@
 import os
 from dotenv import load_dotenv
 from alerts_in_ua import Client as AlertsClient, Client
+from datetime import datetime
 import time
 import msvcrt
 
 load_dotenv()
 TOKEN = os.getenv("ALERTS_IN_UA_TOKEN")
+timeout = 8
+
 if TOKEN:
+    alerts_client: Client = AlertsClient(token=TOKEN)
+    inp = None
     while True:
-        alerts_client: Client = AlertsClient(token=TOKEN)
-        alert_status = alerts_client.get_air_raid_alert_status(31).status
-        # or alert_status = alerts_client.get_air_raid_alert_status('Луганська область')
-        print(alert_status)
-
-        timeout = 5
         startTime = time.time()
-        inp = None
-
         while True:
             if msvcrt.kbhit():
                 inp = msvcrt.getch()
@@ -25,7 +22,9 @@ if TOKEN:
                 break
 
         if inp:
-            print("Exit selected...")
+            print(f"{datetime.now():%Y-%m-%d %H:%M:%S} - {inp} pressed. Exit selected.")
             break
         else:
-            print("Timed out...")
+            alert_status = alerts_client.get_air_raid_alert_status(31).status
+            print(f"{datetime.now():%Y-%m-%d %H:%M:%S} - {alert_status}")
+            print(f"{datetime.now():%Y-%m-%d %H:%M:%S} - Timed out {timeout} sec. Press any key to exit...")
