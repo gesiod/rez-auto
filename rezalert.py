0f4e306abb1488ff7d40378b8e05322e7b77fb83
Grigoriy Gesiod <g.gesiod@gmail.com>
2024-08-22 16:05:36 +0300

    Update rezalert.py

--- rezalert.py
+++ rezalert.py
@@ -1,4 +1,6 @@
 import os
+
+import e
 from dotenv import load_dotenv
 from alerts_in_ua import Client as AlertsClient, Client
 from datetime import datetime
@@ -7,10 +9,10 @@ import msvcrt
 
 load_dotenv()
 TOKEN = os.getenv("ALERTS_IN_UA_TOKEN")
-timeout = 8
+timeout = 15
 
 if TOKEN:
-    alerts_client: Client = AlertsClient(token=TOKEN)
+    alerts_client = AlertsClient(token=TOKEN)
     inp = None
     while True:
         startTime = time.time()
@@ -25,6 +27,18 @@ if TOKEN:
             print(f"{datetime.now():%Y-%m-%d %H:%M:%S} - {inp} pressed. Exit selected.")
             break
         else:
-            alert_status = alerts_client.get_air_raid_alert_status(31).status
-            print(f"{datetime.now():%Y-%m-%d %H:%M:%S} - {alert_status}")
+            if alerts_client != 0:
+                try:
+                    alert_kyiv_status = alerts_client.get_air_raid_alert_status(31)
+                    alert_donetsk_status = alerts_client.get_air_raid_alert_status(28)
+                except:
+                    print("Skip...")
+                    print("Skip...")
+                    print("Skip...")
+                    print("Skip...")
+                    print("Skip...")
+                    print("Skip...")
+                if alert_kyiv_status and alert_donetsk_status:
+                    print(f"{datetime.now():%Y-%m-%d %H:%M:%S} - {alert_kyiv_status.location_title}: is_no_alert = {alert_kyiv_status.is_no_alert()}")
+                    print(f"{datetime.now():%Y-%m-%d %H:%M:%S} - {alert_donetsk_status.location_title}:  is_no_alert = {alert_donetsk_status.is_no_alert()}")
             print(f"{datetime.now():%Y-%m-%d %H:%M:%S} - Timed out {timeout} sec. Press any key to exit...")
